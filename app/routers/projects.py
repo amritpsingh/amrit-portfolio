631844c8c2171f2ba1b3d4b91757f40957b955cb
Amrit Pal Singh <amritps28@gmail.com>
2024-09-29 22:29:11 +1300

    Add projects and updated project page

--- app/routers/projects.py
+++ app/routers/projects.py
@@ -1,10 +1,23 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import yaml
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
+def load_yaml_data(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+    
+
 @router.get("/", response_class=HTMLResponse)
 async def projects(request: Request):
-    return templates.TemplateResponse("projects.html", {"request": request})
+
+    # Load project data
+    projects = load_yaml_data('content/_projects.yml')
+
+    return templates.TemplateResponse("projects.html", {
+        "request": request,
+        "projects": projects
+        })
